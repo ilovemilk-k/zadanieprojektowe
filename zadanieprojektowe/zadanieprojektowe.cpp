c0c1f3f53d5c3342588744f3866f024d94297b70
ilovemilk <179916@prz.edu.pl>
2025-01-23 18:50:09 +0100

    Wersja 1 programu

--- zadanieprojektowe/zadanieprojektowe.cpp
+++ zadanieprojektowe/zadanieprojektowe.cpp
@@ -4,52 +4,30 @@
 #include <vector>
 using namespace std;
 
-int main() {
-	/*
-	int tab[] = { 3, 1, -5, 0, 2, 1 };
-	int rozmiar = sizeof(tab) / sizeof(tab[0]);
-	cout << "\nSize: " << rozmiar<<endl;
-
-	for (int i = 0; i < rozmiar - 2; i++) {
+void wyszukaj(int tab[], int n) {
+	for (int i = 0; i < n - 2; i++) {
 		if (tab[i] + tab[i + 1] < tab[i + 2]) {
 			cout << "[" << tab[i] << ", " << tab[i + 1] << "]\n";
 		}
 	}
-	*/
-	int licznik = 0;
+}
+int main() {
+
+
 	const int size = 100;
 	int tab[size];
-	int pary[size - 2][2];
 	srand(time(NULL));
 	for (int i = 0; i < size; i++) {
 		tab[i] = rand()%100;
 	}
-	cout << endl;
-
-	vector<pair<int, int>>pary2;
-
-	for (int i = 0; i < size - 2; i++) {
-		if (tab[i] + tab[i + 1] >= tab[i + 2] && tab[i + 1] + tab[i + 2] >= tab[i + 3])
-		{
-			i = i + 2;
-		}
-		if (tab[i] + tab[i + 1] < tab[i + 2]) {
-			pary2.push_back(make_pair(tab[i], tab[i + 1]));
-		}
+	for (int i = 0; i < size; i++) {
+		cout<<tab[i]<<" ";
 	}
-		cout << "Pary: \n";
-		for (auto p : pary2) {
-			cout << p.first <<" "<<p.second<< endl;
-		}
-
-
-		//for (int i = 0; i <= licznik; i++) {
-			//cout << pary[i][0]<<" "<< pary[i][1]<<endl;
-			 
-	
-	
-	// Program zeby dzialal dla dowolnej tablicy musi uwzglednic
-	//	1. Liczba w tablicy elementow powinna byc przynajmniej 3
-	//	2. 
+	cout << endl;
+	wyszukaj(tab, size);
+ 
 	return 0;
 }
+// Program zeby dzialal dla dowolnej tablicy musi uwzglednic
+	//	1. Liczba w tablicy elementow powinna byc przynajmniej 3
+	//	2.
